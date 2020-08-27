c14119a69ac5e4f37d6914d9cab15f0947bfaf05
Anthony Accomazzo <accomazz@gmail.com>
2020-08-26 17:11:18 -0700

    feat: add uppercase

--- __tests__/server.spec.ts
+++ __tests__/server.spec.ts
@@ -14,6 +14,9 @@ describe('server', () => {
   it('reverses', done => {
     jsonist.get(urlBase + '/reverse/hello', (_, body) => {
       expect(body.msg).toEqual('olleh');
+  it('responds uppercase', done => {
+    jsonist.get(urlBase + '/uppercase?msg=hello', (_, body) => {
+      expect(body.msg).toEqual('HELLO');
       done();
     });
   });
